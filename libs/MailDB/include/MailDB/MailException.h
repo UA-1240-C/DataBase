7aec262758908e4d00e2bee32d39a5044621daeb
Zikkend <denzik04@gmail.com>
2024-08-13 16:57:26 +0300

    add constructor that receive string rvalue

--- libs/MailDB/include/MailDB/MailException.h
+++ libs/MailDB/include/MailDB/MailException.h
@@ -3,13 +3,15 @@
 #include <string>
 #include <exception>
 
+#include <iostream>
+
 namespace ISXMailDB
 {
 class MailException : public std::exception
 {
 public:
 	MailException(const std::string& message) : m_message(message) {}
-	MailException(const char* message) : m_message(message) {}
+	MailException(std::string&& message) : m_message(std::move(message)) {}
 
 	const char* what() const noexcept override {
         return m_message.c_str();
