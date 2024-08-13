12dd233408219aab1ef07dc64526dbdf2eb7fcf0
Zikkend <denzik04@gmail.com>
2024-08-13 19:43:03 +0300

    clear main

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,5 @@ using namespace ISXMailDB;
 
 int main()
 {
-    using test = tuple<string, string, string>;
-    vector<test> vec, vec2;
-    vec.push_back(test("user2", "subject", "body"));
-    // = {
-    //     test("user2", "subject", "body")
-    // };
-    std::vector<Mail> resutl_mails;
-    for (auto& [sender, subject, body] : vec)
-    {
-        resutl_mails.emplace_back("user1" ,sender, subject, body);
-        cout << sender << subject << body << endl;
-    }
-    for (auto& [sender, subject, body] : vec)
-    {
-        cout << sender << subject << body << endl;
-    }
-    for (const auto& mail : resutl_mails)
-    {
-        cout << mail << endl;
-    }
-
-
+    return 0;
 }
